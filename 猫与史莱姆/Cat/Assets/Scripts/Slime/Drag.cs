5384272053985d4ae460f9c56ce1594e5143f466
Ajay <451303404@qq.com>
2022-05-08 15:17:29 +0800

    史莱姆移动

---
+++ 猫与史莱姆/Cat/Assets/Scripts/Slime/Drag.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+        Vector3 dist;
+        float posX;
+        float posY;
+
+        Vector3 Tmp_curPos;
+        Vector3 Tmp_worldPos;
+
+
+        private Vector3 curPos;
+        private Vector3 worldPos;
+
+        private bool CanDraw = true;
+
+
+        void OnMouseDown()
+        {
+            dist = Camera.main.WorldToScreenPoint(transform.position);
+            posX = Input.mousePosition.x - dist.x;
+            posY = Input.mousePosition.y - dist.y;
+           
+            CanDraw = true;
+           
+            
+        }
+
+        void OnMouseDrag()
+        {
+            if (CanDraw)
+            {
+                curPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY, dist.z);
+                worldPos = Camera.main.ScreenToWorldPoint(curPos);
+                transform.position = worldPos;
+            }
+            
+        }
+
+
+        private  void OnCollisionEnter2D(Collision2D other) {
+            //Debug.Log("鼠标就不该动了");
+            CanDraw = false;
+            // Tmp_worldPos = worldPos;
+            // Tmp_curPos = curPos;
+            // Cursor.lockState = CursorLockMode.Locked;
+            // transform.position = Tmp_worldPos;
+        }
+
+        private void OnCollisionExit2D(Collision2D other) {
+            // Cursor.lockState = CursorLockMode.None;
+            // transform.position = Tmp_worldPos;
+            
+            CanDraw = true;
+            
+        }
+
+        private void OnCollisionStay2D(Collision2D other)
+        {   
+            Debug.Log("还在碰撞");
+            Start();
+        }
+
+        private void Start()
+        {
+            StartCoroutine("DelayFunc");
+        }
+        IEnumerator DelayFunc()
+        {
+            //double delay = 0.1;
+            yield return new WaitForSeconds(0.05f);
+            CanDraw = false;
+            //yield return new WaitForSeconds(delay);
+        }
+}
